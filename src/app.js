d2e51df3fbd1db724e3faeb53f80acf2e4a89410
raojj <471412450@qq.com>
2018-11-15 19:53:30 +0800

    添加单元测试

--- src/app.js
+++ src/app.js
@@ -5,14 +5,37 @@ import Icon from "./icon"
 import ButtonGroup from "./button-group"
 
 
-
 Vue.component("w-button", Button)
 Vue.component("w-icon", Icon)
 Vue.component("w-group", ButtonGroup)
 
 new Vue({
     el: "#app",
-    data:{
+    data: {
         loading1: false
     }
 })
+
+
+// 单元测试
+import chai from 'chai'
+import spies from 'chai-spies'
+
+chai.use(spies)
+
+const expect = chai.expect
+{
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData: {
+            iconName: 'setting'
+        }
+    })
+    vm.$mount()
+    let useElement = vm.$el.querySelector('use')
+    let href = useElement.getAttribute('xlink:href')
+    expect(href).to.eq('#icon-setting')
+    vm.$el.remove()
+    vm.$destroy()
+}
+
