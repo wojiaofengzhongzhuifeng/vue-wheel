9fc723b0dc729980d81e8fac37efd70608f9102e
raojj <471412450@qq.com>
2018-12-04 21:25:26 +0800

    功能: col 组件有几种宽度选择

--- src/col.vue
+++ src/col.vue
@@ -20,7 +20,77 @@
                     let valid = true
                     Object.keys(obj).forEach((key)=>{
                         if(!(["span", "offset"].indexOf(key) > -1)){
-                            console.error(`col组件错误, 输入了${key}, 只能在span 和offset选择`)
+                            console.error(`col组件phone属性错误, 输入了${key}, 只能在span 和offset选择`)
+                            valid = false;
+                        }
+                    });
+                    return valid;
+                }
+            },
+            ipad: {
+                type: Object,
+                validator(obj){
+                    // 确保obj的key是span或者offset, 否则报错
+                    let valid = true
+                    Object.keys(obj).forEach((key)=>{
+                        if(!(["span", "offset"].indexOf(key) > -1)){
+                            console.error(`col组件ipad属性错误, 输入了${key}, 只能在span 和offset选择`)
+                            valid = false;
+                        }
+                    });
+                    return valid;
+                }
+            },
+            narrowPc: {
+                type: Object,
+                validator(obj){
+                    // 确保obj的key是span或者offset, 否则报错
+                    let valid = true
+                    Object.keys(obj).forEach((key)=>{
+                        if(!(["span", "offset"].indexOf(key) > -1)){
+                            console.error(`col组件narrowPc属性错误, 输入了${key}, 只能在span 和offset选择`)
+                            valid = false;
+                        }
+                    });
+                    return valid;
+                }
+            },
+            pc: {
+                type: Object,
+                validator(obj){
+                    // 确保obj的key是span或者offset, 否则报错
+                    let valid = true
+                    Object.keys(obj).forEach((key)=>{
+                        if(!(["span", "offset"].indexOf(key) > -1)){
+                            console.error(`col组件pc属性错误, 输入了${key}, 只能在span 和offset选择`)
+                            valid = false;
+                        }
+                    });
+                    return valid;
+                }
+            },
+            widthPc: {
+                type: Object,
+                validator(obj){
+                    // 确保obj的key是span或者offset, 否则报错
+                    let valid = true
+                    Object.keys(obj).forEach((key)=>{
+                        if(!(["span", "offset"].indexOf(key) > -1)){
+                            console.error(`col组件widthPc属性错误, 输入了${key}, 只能在span 和offset选择`)
+                            valid = false;
+                        }
+                    });
+                    return valid;
+                }
+            },
+            widthWidthPc: {
+                type: Object,
+                validator(obj){
+                    // 确保obj的key是span或者offset, 否则报错
+                    let valid = true
+                    Object.keys(obj).forEach((key)=>{
+                        if(!(["span", "offset"].indexOf(key) > -1)){
+                            console.error(`col组件widthWidthPc属性错误, 输入了${key}, 只能在span 和offset选择`)
                             valid = false;
                         }
                     });
@@ -37,14 +107,23 @@
                 }
             },
             colClasses(){
-                console.log(this.phone);
-                const { span, offset, align, phone} = this
+                const { span, offset, align, phone, ipad, narrowPc, pc, widthPc, widthWidthPc} = this
                 return [
                     offset && `offset-${offset}`,
                     span && `span-${span}`,
                     align && `align-${align}`,
-                    phone.span && `span-phone-${phone.span}`,
-                    phone.offset && `offset-phone-${phone.offset}`,
+                    phone && phone.span && `span-phone-${phone.span}`,
+                    phone && phone.offset && `offset-phone-${phone.offset}`,
+                    ipad && ipad.span && `span-ipad-${ipad.span}`,
+                    ipad && ipad.offset && `offset-ipad-${ipad.offset}`,
+                    narrowPc && narrowPc.span && `span-narrowPc-${narrowPc.span}`,
+                    narrowPc && narrowPc.offset && `offset-narrowPc-${narrowPc.offset}`,
+                    pc && pc.span && `span-pc-${pc.span}`,
+                    pc && pc.offset && `offset-pc-${pc.offset}`,
+                    widthPc && widthPc.span && `span-widthPc-${widthPc.span}`,
+                    widthPc && widthPc.offset && `offset-widthPc-${widthPc.offset}`,
+                    widthWidthPc && widthWidthPc.span && `span-widthWidthPc-${widthWidthPc.span}`,
+                    widthWidthPc && widthWidthPc.offset && `offset-widthWidthPc-${widthWidthPc.offset}`,
                 ]
             },
         },
@@ -101,14 +180,91 @@
         display: flex;
         justify-content: center;
     }
-    @media (max-width: 576px) {
-        $class: span-phone-;
-        @for $i from 1 through 24 {
+    @media (min-width: 1600px) {
+        $class: span-widthWidthPc-;
+        @for $i from 0 through 24 {
             .#{$class}#{$i} {
                 width: ($i / 24) * 100%;
             }
         }
+        $class: offset-span-widthWidthPc--;
+        @for $n from 0 through 24 {
+            .#{$class}#{$n} {
+                margin-left: ($n / 24) * 100%;
+            }
+        }
     }
+    @media (max-width: 1600px) {
+        $class: span-widthPc-;
+        @for $i from 0 through 24 {
+            .#{$class}#{$i} {
+                width: ($i / 24) * 100%;
+            }
+        }
+        $class: offset-widthPc-;
+        @for $n from 0 through 24 {
+            .#{$class}#{$n} {
+                margin-left: ($n / 24) * 100%;
+            }
+        }
+    }
+    @media (max-width: 1200px) {
+        $class: span-pc-;
+        @for $i from 0 through 24 {
+            .#{$class}#{$i} {
+                width: ($i / 24) * 100%;
+            }
+        }
+        $class: offset-pc-;
+        @for $n from 0 through 24 {
+            .#{$class}#{$n} {
+                margin-left: ($n / 24) * 100%;
+            }
+        }
+    }
+    @media (max-width: 992px) {
+        $class: span-narrowPc-;
+        @for $i from 0 through 24 {
+            .#{$class}#{$i} {
+                width: ($i / 24) * 100%;
+            }
+        }
+        $class: offset-narrowPc-;
+        @for $n from 0 through 24 {
+            .#{$class}#{$n} {
+                margin-left: ($n / 24) * 100%;
+            }
+        }
+    }
+    @media (max-width: 768px) {
+        $class: span-ipad-;
+        @for $i from 0 through 24 {
+            .#{$class}#{$i} {
+                width: ($i / 24) * 100%;
+            }
+        }
+        $class: offset-ipad-;
+        @for $n from 0 through 24 {
+            .#{$class}#{$n} {
+                margin-left: ($n / 24) * 100%;
+            }
+        }
+    }
+    @media (max-width: 576px) {
+        $class: span-phone-;
+        @for $i from 0 through 24 {
+            .#{$class}#{$i} {
+                width: ($i / 24) * 100%;
+            }
+        }
+        $class: offset-phone-;
+        @for $n from 0 through 24 {
+            .#{$class}#{$n} {
+                margin-left: ($n / 24) * 100%;
+            }
+        }
+    }
+
 
 
 
