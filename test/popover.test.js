569dbd674474dd8efe46c3b92a63e6237c15a449
raojj <471412450@qq.com>
2018-12-28 19:21:09 +0800

    第二个测试用例

--- test/popover.test.js
+++ test/popover.test.js
@@ -17,32 +17,65 @@ describe('spies', () => {
     it('存在.', () => {
         expect(Popover).to.be.ok
     })
-    it('接受 position 属性', (done) => {
+    // it('接受 position 属性', (done) => {
+    //     Vue.component("w-popover", Popover);
+    //     Vue.component("w-button", Button);
+    //
+    //     let div = document.createElement("div");
+    //     document.body.appendChild(div);
+    //     div.innerHTML = `
+    // <w-popover position="bottom" ref="a">
+    //     <w-button class="test">点击我2</w-button>
+    //     <template slot="content">
+    //         <p>fdjskfldjsa</p>
+    //     </template>
+    // </w-popover>
+    //     `
+    //     let vm = new Vue({
+    //         el: div
+    //     })
+    //
+    //
+    //     vm.$nextTick(() => {
+    //         let button = vm.$refs.a.$refs.button
+    //         button.click();
+    //         setTimeout(() => {
+    //             let contentClass = vm.$refs.a.$refs.content.classList
+    //             expect(contentClass.contains('position-bottom')).to.be.true
+    //             vm.$el.remove()
+    //             vm.$destroy()
+    //             done()
+    //         }, 0)
+    //     })
+    // })
+
+    it('接受 trigger 属性', (done) => {
         Vue.component("w-popover", Popover);
         Vue.component("w-button", Button);
 
         let div = document.createElement("div");
         document.body.appendChild(div);
         div.innerHTML = `
-    <w-popover position="top" >
-        <w-button class="test">点击我2</w-button>
-        <template slot="content">
-            <p>fdjskfldjsa</p>
-        </template>
-    </w-popover>
+            <w-popover trigger="hover" ref="aaaa">
+                <w-button class="test1">2323232</w-button>
+                <template slot="content">
+                    <p>fdjskfldjsa</p>
+                </template>
+            </w-popover>
         `
         let vm = new Vue({
             el: div
         })
 
 
-        vm.$nextTick(()=>{
-            let button = document.body.querySelector(".test")
-            button.click();
-            setTimeout(()=>{
-                let contentClass = document.body.querySelector(".popover-wrapper").classList
-                console.log(contentClass);
-                expect(contentClass.contains('position-top')).to.be.true
+        vm.$nextTick(() => {
+            let button = vm.$refs.aaaa.$refs.button
+            const event = new Event("mouseenter");
+            button.dispatchEvent(event);
+            setTimeout(() => {
+                const {content} = vm.$refs.aaaa.$refs;
+                content.classList.contains(".popover-wrapper")
+                expect(content.classList.contains("popover-wrapper")).to.be.true
                 done()
             }, 200)
         })
