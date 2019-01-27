8a1eccf42a4521ee5144ad924261c1f7f6e2cb6a
raojj <471412450@qq.com>
2019-01-27 22:57:24 +0800

    popover2 content展示,content 位置

--- src/popover.vue
+++ src/popover.vue
@@ -3,6 +3,7 @@
         <div ref="toggleWrapper">
             <slot></slot>
         </div>
+        <!--4报错-->
         <div class="popover-content" :class="classes" v-show="visible" ref="contentWrapper">
             <slot name="content"></slot>
         </div>
@@ -14,6 +15,7 @@
     * 1。 经验bug： 组件上级元素有overflow：hidden，popover content展示不了 => 解决方法： content 放到docuemnt.body，通过css来定位
     * 2。 经验bug： 如果出现滚动条，popover content 内容位置出错
     * 3。 给 popover 添加监听click事件，为什么要这样？ 3监听
+    * 4。 改成 v-if 会报错， 怎么解决？？？ 4报错
     * */
     export default {
         data(){
@@ -34,7 +36,7 @@
         },
         mounted() {
             // 3监听： 为什么这样监听，为什么监听 popover 不是 toggleWrapper？？
-            this.$refs.toggleWrapper.addEventListener('click', ()=>{
+            this.$refs.toggleWrapper.addEventListener('click', (e)=>{
                 if(this.visible){
                     this.hideContent()
                 } else {
@@ -46,9 +48,20 @@
         methods:{
             hideContent(){
                 this.visible = false
+
             },
             showContent(){
                 this.visible = true
+
+                this.positionContent()
+
+            },
+            positionContent(){
+                document.body.appendChild(this.$refs.contentWrapper)
+                const {toggleWrapper,contentWrapper} = this.$refs
+                const {top, left,height} = toggleWrapper.getBoundingClientRect()
+                contentWrapper.style.top = top + height + "px"
+                contentWrapper.style.left = left + "px"
             }
         }
     }
@@ -57,13 +70,16 @@
 <style lang="scss" scoped>
     @import "../styles/var";
     .popover{
+        display: inline-flex;
         &-content{
+            z-index: 1;
+            background: white;
             display: inline-flex;
             box-shadow: $box-shadow;
             min-width:150px;
             border-radius: 4px;
             padding: 5px 16px;
-            position: relative;
+            position: absolute;
             margin-top: 10px;
         }
         &-content::before{
@@ -83,6 +99,14 @@
             border-right: 7px solid transparent;
             border-bottom: 7px solid white;
             position: absolute;
+        }
+        &-content.bottom::before{
+            top: -13px;
+            left: 23px;
+        }
+        &-content.bottom::after{
+            top: -13px;
+            left: 23px;
         }
     }
 </style>
